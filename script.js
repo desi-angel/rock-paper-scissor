7bf51d4d32996460f3a410128821b4bfcc96d8e1
Sameer <sameer_ahmed141@hotmail.com>
2022-05-11 00:44:37 +0500

    add a method to restart the game 1. Create a function to restart the game 2. Create the modal window that prompts to restart the game 3. Use window.location.reload to refresh 4. Add a few animation to the modal window

--- script.js
+++ script.js
@@ -35,27 +35,48 @@ function playRound(computerSelection, playerSelection){     //Playing a single r
             winCount: winCount, loseCount:loseCount};
         }
     }
-function displayResult(e){  //displaying result to the user
-        e.preventDefault();   //Prevents bubbling on the way down
-        let result = playRound(computerPlay(),e.currentTarget.value); //getting result from the game played
-        const msg = document.querySelector('#win-message'); //selecting p element to announce winner of the round
-        const human = document.querySelector('#human'); //Selecting the human score
-        const machine = document.querySelector('#computer'); //selecting the machine score 
-        msg.textContent = result.message;
-        human.textContent = 'Man: '+result.winCount;
-        machine.textContent = 'Machine: ' + result.loseCount;
-        setTimeout(removeMsg,2000);
+function displayResult(result){  //displaying result to the user
+    const msg = document.querySelector('#win-message'); //selecting p element to announce winner of the round
+    const human = document.querySelector('#human'); //Selecting the human score
+    const machine = document.querySelector('#computer'); //selecting the machine score 
+    msg.textContent = result.message;
+    human.textContent = 'Man: '+result.winCount;
+    machine.textContent = 'Machine: ' + result.loseCount;
+}
+function check(e){
+    e.preventDefault();   //Prevents bubbling on the way down
+    let game = playRound(computerPlay(),e.currentTarget.value); //getting result from the game played
+    displayResult(game);
+    const container = document.querySelector('#container');
+    const result = document.querySelector('#result');
+    if(winCount === 5){
+        const winner = document.createTextNode('Congratualtions!! You have succeded in defeating Machine');
+        result.appendChild(winner); 
+        playAgain('Wanna Play again ?');
     }
-    
-function removeMsg(){
-        const msg = document.querySelector('#win-message');
-        msg.textContent = ''; 
+    else if(loseCount === 5){
+        const looser = document.createTextNode("You have failed. There's no running from what comes");
+        result.appendChild(looser);
+        playAgain('Try again ?');
     }
-    
+
+}
+function playAgain(prompt_text){
+        const modal = document.querySelector('.modal');
+        const text = document.querySelector('.prompt')
+        const btn = document.querySelector('#modal-btn');
+        text.textContent = prompt_text;
+        modal.style.display = 'block';
+
+        btn.onclick = function(){
+            window.location.reload();
+            
+        }
+}
     let winCount = 0; //initialize the win counter
     let loseCount = 0; //initialize the lose counter 
     const choices = document.querySelectorAll('button'); //Select all the buttons
     choices.forEach(btn =>
-        btn.addEventListener('click', displayResult,{  //Add event listener to each button
+        btn.addEventListener('click', check,{  //Add event listener to each button
             capture: true
     }));
